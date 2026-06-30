f62c7bdefd295540faa535cb9b94e45f8e9c8491
Katarina Vujovic <KVujovic@livingston.com>
2026-06-30 12:19:23 +0200

    Add CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import counter
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+ALLOWED_ORIGIN = os.getenv("FRONTEND_URL", "http://localhost:3000")
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[ALLOWED_ORIGIN],
+    allow_methods=["*"],
+)
 
 app.include_router(counter.router)
